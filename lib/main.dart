655fa434a0e26ccdb51af1f41348c8e6f850c6c3
Chirag Yadav <yadavchirag455@gmail.com>
2024-06-04 16:06:43 +0530

    Change App Name

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        title: 'Chat App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
